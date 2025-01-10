9a0570d5027ef6bfcc26e7120a2b7ea1d9cf6062
이현수 <mytime0316@naver.com>
2025-01-10 17:57:58 +0900

    Lv1 수정 완료

--- calculator/src/main/java/com/example/calculator/Calculator.java
+++ calculator/src/main/java/com/example/calculator/Calculator.java
@@ -6,46 +6,42 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // 양의 정수 입력 받기
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNum = sc.nextInt();
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int secondNum = sc.nextInt();
+        // 사칙연산 기호 입력 받기
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
+
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int A = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int B = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char C = sc.next().toCharArray()[0];
-
-            double DA = A;
-            double DB = B;
-
-            while (true) {
-                if (C == '+') {
-                    System.out.println("정답은 " + (A+B) + "입니다.");
-                    break;
-                } else if (C == '-') {
-                    System.out.println("정답은 " + (A-B) + "입니다.");
-                    break;
-                } else if (C == '*') {
-                    System.out.println("정답은 " + (A*B) + "입니다.");
-                    break;
-                } else if (C == '/') {
-                    if (B == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        break;
-                    } else {
-                        System.out.println("정답은 " + (DA/DB) + "입니다.");
-                        break;
-                    }
-                } else if (C != '+' || C != '-' || C != '*' || C != '/') {
-                    System.out.println("잘못된 기호를 입력하셨습니다.");
-                    break;
-                } else {
-                    System.out.println("잘못된 숫자를 입력하셨습니다.");
-                    break;
+            // 연산 진행후 결과값을 출력
+            int result;
+            if (operator == '+') {
+                result = firstNum + secondNum;
+                System.out.println("결과값은 " + result + " 입니다");
+            } else if (operator == '-') {
+                result = firstNum - secondNum;
+                System.out.println("결과값은 " + result + " 입니다");
+            } else if (operator == '*') {
+                result = firstNum * secondNum;
+                System.out.println("결과값은 " + result + " 입니다");
+            } else if (operator == '/') {
+                if (secondNum == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                 }
+                result = firstNum / secondNum;
+                System.out.println("결과값은 " + result + " 입니다");
+            } else {
+                System.out.println("잘못된 연산기호를 입력하였습니다.");
             }
-            System.out.print("계속 계산하시겠습니까? (exit 입력 시 종료): ");
-            String D = sc.next();
-            if (D.equals("exit")) {
+
+            // exit를 입력하기 전까지 무한으로 반복
+            System.out.print("계속 계산 하시겠습니까? (exit 입력 시 종료): ");
+            String next = sc.next();
+            if ("exit".equals(next)) {
+                System.out.println("계산을 종료합니다.");
                 break;
             }
         }
